37f089b42ffca899860e9998bd43156385d0fb45
Martin House <torchester@gmail.com>
2023-03-07 06:17:43 +0500

    upd django files

--- arco/arco/asgi.py
+++ arco/arco/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for webpage project.
+ASGI config for arco project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'webpage.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'arco.settings')
 
 application = get_asgi_application()
